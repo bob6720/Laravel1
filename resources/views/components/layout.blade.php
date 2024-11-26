0a0c58dac755771a1248d618bd67beec1894f206
bob6720 <meyerharry0@gmail.com>
2024-11-26 16:00:38 +1300

    2nd stage

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,8 +4,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pixel Positions</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body>
+<body class="bg-black text-white">
     
+        <div class="px-10">
+            <nav class="flex justify-between items-center py-4 border-b border-white/10s">
+                <div>
+                    <a href="/">
+                        <img src="{{ Vite::asset('resources/images/logo.svg') }}" alt="">
+                    </a>
+                </div>
+                <div class="space-x-6 font-bold">
+                    <a href="#">Jobs</a>
+                    <a href="#">Careers</a>
+                    <a href="#">Salaries</a>
+                    <a href="#">Companies</a>
+                </div>
+                <div>
+                    <a href="">Post a Job</a>
+                </div>
+            </nav>
+
+            <main class="mt-10 max-w-[986px]">
+                {{ $slot }}
+            </main>
+        </div>
 </body>
 </html>
